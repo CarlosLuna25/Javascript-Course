4d039ec68a7123229f99588a4dfc17bcc8e0fa70
FamilySystemPC <cl72506@@gmail.com>
2021-03-21 16:30:29 -0400

    fetch Eliminar usuario

--- Ajax/ejemplofetch.js
+++ Ajax/ejemplofetch.js
@@ -4,15 +4,38 @@ let Nombre= document.getElementById("nombre");
 let Apellido=  document.getElementById("apellido")
 let Pais=  document.getElementById("pais")
 let Usuarios=[]
+let BotonesEliminar=[]
 
 refrescar();
 
 function render(){
-    const usuariosRender= Usuarios.map(usuario=> `<tr> <td> ${usuario.nombre} </td>  </tr>`).join("");
+    const usuariosRender= Usuarios.map((usuario, index)=> `<tr> <td> ${usuario.nombre ? usuario.nombre : "Vacio"} </td>
+    <td> ${usuario.apellido ? usuario.apellido : "Vacio"} </td>
+    <td> ${usuario.pais ? usuario.pais : "Vacio"} </td>
+    <td><button class="eliminar" data-index=${index}> Eliminar</button></td>
+    </tr>`).join("");
     console.log(usuariosRender);
+
     lista.innerHTML= usuariosRender;
+    BotonesEliminar= document.getElementsByClassName('eliminar');
+    Array.from(BotonesEliminar).forEach(botonEliminar=>{
+      botonEliminar.onclick= EliminarUsuario   })
 }
 
+function EliminarUsuario(e){
+  e.preventDefault();
+
+  console.log("eliminar usuario", e.target.dataset.index)
+  fetch(`https://bootcamp-dia-3.camilomontoyau.now.sh/usuarios/${e.target.dataset.index}`,{
+    method:"DELETE",
+  }).then((response)=>response.json())
+  .then(respuestaJSON=>{
+    console.log("Response",respuestaJSON)
+
+    });
+    setTimeout(refrescar, 3000)
+  
+}
 
 function EnviarDatos(e) {
     e.preventDefault();
